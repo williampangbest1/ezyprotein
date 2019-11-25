71136ba2731c147bdc16eac9388328810abf37ac
William Pang <williampangbest1@gmail.com>
2019-11-25 14:24:50 -0500

    make it better

---
+++ main.py
@@ -0,0 +1,24 @@
+import flask
+from pair_search import pair_search,pairs_search 
+import json
+
+app = flask.Flask(__name__)
+
+@app.route("/")
+def myIndex():
+	return flask.render_template("index.html")
+
+
+@app.route('/home', methods=['POST'])
+def search():
+	#s = flask.request.values["data"];
+	s=flask.request.get_json();
+	s=s["data"];
+	a=pairs_search(s[0],float(s[1]["value"]));
+	
+	results=pair_search(s);
+	#return flask.render_template("index.html", result=json.dumps(result))
+	return json.dumps(a)
+
+if __name__ == "__main__":
+	app.run(debug=True)
